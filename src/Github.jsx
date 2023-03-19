4f5a6d8d432523d483142da867fb12f8ba604a89
Tinotenda Nyashanau <tinotendanyash@gmail.com>
2023-03-19 17:43:21 +0100

    finalcode

---
+++ src/Github.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+// import "./index.css";
+
+// i sm importing the gihub api users to my projects
+const url = "https://api.github.com/users";
+
+const GitHubUsers = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <>
+      <h1>Github users</h1>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default GitHubUsers;
